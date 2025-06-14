a4f3f43dd333eabce14a4640c5ef753cd2e74f66
Vladimir Fokow <57260995+VladimirFokow@users.noreply.github.com>
2025-06-15 00:21:28 +0200

    version fix

--- my_notes/triton_my_notes.py
+++ my_notes/triton_my_notes.py
@@ -4,8 +4,8 @@ import triton.language as tl
 
 # See GPU specifications:
 # -----------------------
-DEVICE = triton.runtime.driver.active.get_active_torch_device()  # device(type='cuda', index=0)
-# DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')  # device(type='cuda', index=0)
+# DEVICE = triton.runtime.driver.active.get_active_torch_device()  # (for triton>= 3.2) device(type='cuda', index=0)
+DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')  # device(type='cuda', index=0)
 triton.runtime.driver.active.get_current_target().backend  # 'cuda'
 triton.runtime.driver.active.get_current_target().arch  # 120  --> on RTX 5060 Ti
 
